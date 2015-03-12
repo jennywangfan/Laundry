fb7df37a1e334a1a61bd254f2406efb887d283c7
jennywangfan <jennywangfan@gmail.com>
2015-03-11 22:49:29 -0400

    20150311

---
+++ src/main/java/com/triplexilaundry/extjsdata/ComboboxModel.java
@@ -0,0 +1,36 @@
+/**
+* <p>Title: ComboboxModel.java</p>
+* <p>Description: </p>
+* <p>Copyright: Copyright (c) 2015</p>
+* <p>All Right Reserved</p>
+* @author Fan Wang
+* @date Mar 11, 2015
+* @version 1.0
+*/
+package com.triplexilaundry.extjsdata;
+
+/**
+ * <p>Title: ComboboxModel</p>
+ * <p>Description: </p>
+ * <p>All Right Reserved</p> 
+ * @author Fan Wang
+ * @date Mar 11, 2015
+ */
+public class ComboboxModel {
+	private String name;
+	private int attribute;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAttribute() {
+		return attribute;
+	}
+	public void setAttribute(int attribute) {
+		this.attribute = attribute;
+	}
+	
+
+}
